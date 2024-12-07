5b99f8238cd4be08c8335dff499432382b11518f
CyberClown-png <84931583+CyberClown-png@users.noreply.github.com>
2024-12-07 04:02:29 +0200

    fixed problems with acces to UI childrens

--- gameManager.gd
+++ gameManager.gd
@@ -1,5 +1,11 @@
 extends Node3D
 
 func _ready():
-	InventoryManager.set_inventory_container($SubViewportContainer/SubViewport/CanvasLayer/Control/HBoxContainer)
+# Получаем ссылку на SubViewportContainer и затем на SubViewport
+	var canvas = $SubViewportContainer/SubViewport/UI
+	var control = canvas.get_node("Control2")
+	var hbox = control.get_node("HBoxContainer")
+
+	InventoryManager.set_inventory_container(hbox)
+	
 	InventoryManager._initialize_slots()
